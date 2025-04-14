6a3fdb73d9c3fa0877faa4659fa9b8b9f0662d19
fernandez99fc <fernandezoluwajuwonlo@gmail.com>
2025-04-14 20:41:14 +0100

    Update Payload_1.js

--- Payload_1.js
+++ Payload_1.js
@@ -1,4 +1,4 @@
-// Changes the text content of an "h2" and "h1" tag.
+// Changes the text content of an "h2" and "h1" tag with the position of [2] for h2 and [0] for h1
 
 <script>
 document.getElementsByTagName("h2")[2].innerHTML = "Modified You";
