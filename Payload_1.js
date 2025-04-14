9b4b229e78b7c2a59a563fd7fddc17daf4acb157
fernandez99fc <fernandezoluwajuwonlo@gmail.com>
2025-04-14 20:28:53 +0100

    Create Payload_1.js

---
+++ Payload_1.js
@@ -0,0 +1,7 @@
+// Changes the text content of an "h2" and "h1" tag.
+
+<script>
+document.getElementsByTagName("h2")[2].innerHTML = "Modified You";
+
+document.getElementsByTagName("h1")[0].innerHTML = "Found You";
+</script>
